72ecea6405111f20788b8368391f57c8c0a6b34f
MrCream0 <slamboi666@gmail.com>
2023-03-24 12:54:17 -0500

    front end now displays info from database and lets you add and delete objects  however id has to be entered as 0 to add new items unsure why atm

--- InventoryMgr/ClientApp/src/AppRoutes.js
+++ InventoryMgr/ClientApp/src/AppRoutes.js
@@ -1,11 +1,12 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
+
+import Table from "./components/Home";
 
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
+    element: <Table />
   },
   {
     path: '/counter',
@@ -14,7 +15,7 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
-  }
+  },
 ];
 
 export default AppRoutes;
--- InventoryMgr/ClientApp/src/components/Home.js
+++ InventoryMgr/ClientApp/src/components/Home.js
@@ -1,93 +1,138 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
+
+import axios from "axios";
 
 
 //make table for items then add buttons and stuff for on hand in transit and add options for adding new items into the table and deleting items from the table
 
-export class Home extends Component {
-  static displayName = Home.name;
+function Table() {
+  const [data, setData] = useState([]);
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [onHand, setAmount] = useState("");
 
-  //invObjects
-  state = {
-    inventory: [
-      { name: "Item 1", onHand: 10, inTransit: 5 },
-      { name: "Item 2", onHand: 20, inTransit: 10 },
-      { name: "Item 3", onHand: 15, inTransit: 0 },
-    ]
+  // Fetch data from backend API on component mount
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await axios.get("https://localhost:7216/api/product");
+    setData(response.data);
   };
 
-
-  addInventoryObject = (name, onHand, inTransit) =>{//add option for adding new items into the table
-    const nameInput = document.getElementById('name').value;
-    const onHandInput = document.getElementById('onHand').value;
-    const inTransitInput = document.getElementById('inTran').value;
-
-    name = nameInput;
-    onHand = parseInt(onHandInput);
-    inTransit = parseInt(inTransitInput);
-
-    const newInventory = [...this.state.inventory];
-    newInventory.push({name, onHand, inTransit});
-    this.setState({inventory: newInventory});
-
-    document.getElementById('name').value = '';
-    document.getElementById('onHand').value = '';
-    document.getElementById('inTran').value = '';
+  const handleAdd = async () => {
+    // Make POST request to backend API to add new data to database
+    await axios.post("https://localhost:7216/api/product", { id, name, description, onHand });
+    // Fetch updated data from backend API
+    await fetchData();
+    // Clear input fields
+    setId("");
+    setName("");
+    setDescription("");
+    setAmount("");
   };
 
-
-
-  updateOnHand = (index, value) => {
-    const newInventory = [...this.state.inventory];
-    newInventory[index].onHand = value;
-    this.setState({ inventory: newInventory });
-  }; // add option for udpating on hand and also showing the confirmed on hand value "to avoid messing with the on hand accidently"
-
-  updateInTransit = (index, value) => {
-    const newInventory = [...this.state.inventory];//create a new array with updated values
-    newInventory[index].inTransit = value;
-    this.setState({ inventory: newInventory });//update state
+  const handleUpdate = async (id) => {
+    // Make PUT request to backend API to update data in database
+    await axios.put(`https://localhost:7216/api/product/${id}`, { name, description, onHand });
+    // Fetch updated data from backend API
+    await fetchData();
   };
 
-  render() {
-    return (
-      <div>
-        <h1>Inventory Management</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>On Hand</th>
-              <th>In Transit</th>
+  const handleDelete = async (id) => {
+    // Make DELETE request to backend API to delete data from database
+    await axios.delete(`https://localhost:7216/api/product/${id}`);
+    // Fetch updated data from backend API
+    await fetchData();
+  };
+
+  return (
+    <div class="body">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>On Hand</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>
+                <input
+                  type="text"
+                  value={item.name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  value={item.description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  value={item.onHand}
+                  onChange={(e) => setAmount(e.target.value)}
+                />
+              </td>
+              <td>
+                <button onClick={() => handleUpdate(item.id)}>Update</button>
+                <button onClick={() => handleDelete(item.id)}>Delete</button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {this.state.inventory.map((item, index) => (
-              <tr key={index}>
-                <td>{item.name}</td>
-                <td>
-                  <input
-                    type="number"
-                    value={item.onHand}
-                    onChange={(e) => this.updateOnHand(index, e.target.value)}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="number"
-                    value={item.inTransit}
-                    onChange={(e) => this.updateInTransit(index, e.target.value)}
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <input id='name' type="text" placeholder="Item Name" />
-        <input id='onHand' type="number" placeholder="On Hand" />
-        <input id='inTran' type="number" placeholder="In Transit" />
-        <button onClick={() => this.addInventoryObject()}>Add Item</button>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        <h2>Add new data</h2>
+        <div>
+          <label>ID:</label>
+          <input
+            type="text"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Description:</label>
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Amount:</label>
+          <input
+            type="number"
+            value={onHand}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <div>
+            <button onClick={() => handleAdd()}>Add</button>
+          </div>
+        </div>
       </div>
-    );
-  };
+
+    </div>);
 };
+export default Table;
